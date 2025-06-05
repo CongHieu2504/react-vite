785b20b2bf4e00386679f65b631ca0c14e4e4235
Cong Hieu <hieuho250403@gmail.com>
2025-06-05 18:20:14 +0700

    Video 34: Props là 1 biến Object va cach truyen bien Props

--- 01-react-vite-hoidanit-starter-master/src/App.jsx
+++ 01-react-vite-hoidanit-starter-master/src/App.jsx
@@ -4,11 +4,24 @@ import TodoNew from './components/todo/TodoNew'
 import ReactLogo from './assets/react.svg'
 
 const App = () => {
+
+  const name = "Cong Hieu"
+  const age = 22
+  const address = {
+    city: "Ho Chi Minh",
+    country: "Viet Name"
+  }
+
+
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={name}
+        age={age}
+        address={address}
+      />
       <div className='todo-image'>
         <img src={ReactLogo} className='logo' />
       </div>
--- 01-react-vite-hoidanit-starter-master/src/components/todo/TodoData.jsx
+++ 01-react-vite-hoidanit-starter-master/src/components/todo/TodoData.jsx
@@ -1,6 +1,16 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    // props là một biến Object
+
+    // const { name, age, address } = props;
+    const name = props.name;
+    const age = props.age;
+    const address = props.address;
+
+    console.log(">>> Check pros: ", props)
     return (
+        // cach truyen bien props
         <div className='todo-data'>
+            <div>My name is {props.name}</div>
             <div>Learning React</div>
             <div>Watching Youtube</div>
         </div>
