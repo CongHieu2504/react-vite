71ea4cedbd4c92adac25b6e5e4fe27ec0d8620f0
Cong Hieu <hieuho250403@gmail.com>
2025-06-06 10:02:32 +0700

    Video 36 Event Onchange

--- 01-react-vite-hoidanit-starter-master/src/components/todo/TodoNew.jsx
+++ 01-react-vite-hoidanit-starter-master/src/components/todo/TodoNew.jsx
@@ -2,11 +2,26 @@ const TodoNew = (props) => {
     console.log(">>> Check point", props)
     const { addNewTodo } = props;
 
-    addNewTodo();
+    // addNewTodo("Hieu");
+
+    const handleClick = () => {
+        alert("Click me");
+    }
+
+    // Thêm sự kiện event.target.value: để lấy giá trị của input
+    const handleOnChange = (name) => {
+        // console.log(">>> handleOnchange", event.target.value);
+        console.log(">>> handleOnchange", name);
+    }
+
     return (
         <div className='todo-new'>
-            <input type="text" />
-            <button>Add</button>
+            {/* <input type="text" onChange={handleOnChange} /> */}
+            <input type="text" onChange={() => handleOnChange(event.target.value)} />
+            <button
+                style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
