acbfc2e014860602fc8b1152efc8eaa253f32d37
Cong Hieu <hieuho250403@gmail.com>
2025-06-09 16:04:44 +0700

    Video 45 cách dùng router

--- 01-react-vite-hoidanit-starter-master/src/main.jsx
+++ 01-react-vite-hoidanit-starter-master/src/main.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+
+  {
+    path: "/login",
+    element: <div>login Page</div>
+  },
+
+  {
+    path: "/register",
+    element: <div>register Page</div>
+  },
+
+  {
+    path: "/users",
+    element: <div>user Page</div>
+  },
+
+  {
+    path: "/products",
+    element: <div>product Page</div>
+  },
+]);
+
+// RouterProvider dùng để điều huớng các route trong ứng dụng React
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
