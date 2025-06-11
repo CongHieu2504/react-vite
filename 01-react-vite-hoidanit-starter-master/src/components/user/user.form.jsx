8779addda407dd9c634c1abe4ee33a6e0975cc4f
Cong Hieu <hieuho250403@gmail.com>
2025-06-11 16:28:27 +0700

    Video 70 + 71

--- 01-react-vite-hoidanit-starter-master/src/components/user/user.form.jsx
+++ 01-react-vite-hoidanit-starter-master/src/components/user/user.form.jsx
@@ -1,6 +1,6 @@
-import { Button, Input } from "antd";
+import { Button, Input, notification } from "antd";
 import { useState } from "react";
-import axios from "axios";
+import { createUserAPI } from "../../services/api.service";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("");
@@ -9,16 +9,23 @@ const UserForm = () => {
     const [phone, setPhone] = useState("");
 
     // hàm onClick
-    const handleClickBtn = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone
+    const handleClickBtn = async () => {
+        const res = await createUserAPI(fullName, email, password, phone);
+        // sử dụng ANTD để hiển thị thông báo: Notification
+
+        if (res.data) {
+            notification.success({
+                message: "Create user",
+                description: "Tạo user thành công"
+            })
         }
-        axios.post(URL_BACKEND, data);
-        console.log(">>> handleClickBtn", { fullName, email, password, phone });
+        else {
+            notification.error({
+                message: "Error create user",
+                description: JSON.stringify(res.message)
+            })
+        }
+        console.log("API response:", res);
     }
 
     return (
